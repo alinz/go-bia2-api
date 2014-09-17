1eab4bb0fbd871fafac5fa106005ee8d787dc132
Ali Najafizadeh <a.najafizadeh@gmail.com>
2014-09-17 18:45:27 -0400

    added more filters

--- static/app/js/controller.js
+++ static/app/js/controller.js
@@ -1,14 +1,39 @@
 angular.module('app.controllers')
   .controller("homeCtrl", ["$scope", "restCalls", function ($scope, restCalls) {
+    $scope.searchValue = "";
     $scope.searchType = "artist";
 
-    $scope.$watch("search", function (value) {
-      if (value && value.length > 0) {
-        restCalls.search(value, $scope.searchType).then(function (results) {
+    $scope.doSearchQuery = function () {
+      if ($scope.searchValue && $scope.searchValue.length > 0) {
+        restCalls.search($scope.searchValue, $scope.searchType).then(function (results) {
+          switch ($scope.searchType) {
+            case "artist":
+              break;
+            case "album":
+              results = results.map(function (artist) {
+                artist.albums = artist.albums.filter(function (album) {
+                  return album.title.toLowerCase().indexOf($scope.searchValue.toLowerCase()) != -1;
+                });
+                return artist;
+              });
+              break;
+            case "track":
+              results = results.map(function (artist) {
+                artist.albums = artist.albums.filter(function (album) {
+                  album.tracks = album.tracks.filter(function (track) {
+                    return track.name.toLowerCase().indexOf($scope.searchValue.toLowerCase()) != -1;
+                  });
+                  return album.tracks.length != 0;
+                });
+                return artist;
+              });
+              break;
+          }
+
           $scope.artists = results;
         });
       } else {
         $scope.artists = [];
       }
-    });
+    };
   }]);
