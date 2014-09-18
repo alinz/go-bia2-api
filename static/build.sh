f09345be0105f8895be66c0747436461c71be198
Ali Najafizadeh <a.najafizadeh@gmail.com>
2014-09-18 15:57:40 -0400

    update build scrip to move fonts folder

--- static/build.sh
+++ static/build.sh
@@ -1,1 +1,1 @@
-npm install; gulp; rm -rf app; rm -rf node_modules; rm build.sh gulpfile.js package.json
+npm install; gulp; mv app/fonts .; rm -rf app; rm -rf node_modules; rm build.sh gulpfile.js package.json
